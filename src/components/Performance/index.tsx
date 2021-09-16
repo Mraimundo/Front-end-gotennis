efd421d9f7a609803192710eb5ec49f65a801c69
MFraimundo <raimundo.mouzinho@teamsys.com.br>
2021-09-16 00:02:50 -0300

    linerar chart

--- src/components/Performance/index.tsx
+++ src/components/Performance/index.tsx
@@ -20,9 +20,82 @@ export function Performance () {
             <BiCaretDown/>
           </button>
         </div>
-        <div>
-          {/* {/* <Performance/> */}
-          {/* <Graphics/>  */}
+        <div className={styles.rechartsContent}>
+          <div className={styles.areaChart} >
+
+          </div>
+          <div className={styles.linearChart} >
+            <div className={styles.progressContainer}>
+              <div className={styles.progress_Content}>
+                <div className={styles.progress_won}>
+                  <p>120</p>
+                </div>
+                <div className={styles.progress_lose}>
+                  <p>80</p>
+                </div>
+              </div>
+              <div>
+                <p>200</p>
+                <span>jogos</span>
+              </div>
+            </div>
+            <div className={styles.progressContainer}>
+              <div className={styles.progress_Content}>
+                <div className={styles.progress_won}>
+                  <p>100</p>
+                </div>
+                <div className={styles.progress_lose}>
+                  <p>40</p>
+                </div>
+              </div>
+              <div>
+                <p>140</p>
+                <span>jogos</span>
+              </div>
+            </div>
+            <div className={styles.progressContainer}>
+              <div className={styles.progress_Content}>
+                <div className={styles.progress_won}>
+                  <p>120</p>
+                </div>
+                <div className={styles.progress_lose}>
+                  <p>80</p>
+                </div>
+              </div>
+              <div>
+                <p>200</p>
+                <span>jogos</span>
+              </div>
+            </div>
+            <div className={styles.progressContainer}>
+              <div className={styles.progress_Content}>
+                <div className={styles.progress_won}>
+                  <p>100</p>
+                </div>
+                <div className={styles.progress_lose}>
+                  <p>40</p>
+                </div>
+              </div>
+              <div>
+                <p>140</p>
+                <span>jogos</span>
+              </div>
+            </div>
+            <div className={styles.progressContainer}>
+              <div className={styles.progress_Content}>
+                <div className={styles.progress_won}>
+                  <p>120</p>
+                </div>
+                <div className={styles.progress_lose}>
+                  <p>80</p>
+                </div>
+              </div>
+              <div>
+                <p>300</p>
+                <span>jogos</span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
